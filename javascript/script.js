7a2b779020e4f5b1cc94b56e31abd6fed7d97665
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-25 04:09:49 -0300

    a

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+
+//executa o laço enquanto for menor que 6
+var  c=1
+do{
+    console.log(`Valor de C é ${c}`)
+    c++
+}while(c <= 6)
+// caso passe de 6 apresenta as mensagens abaixo
+if(c >= 6){
+    console.log('Você atingiu o limite')
+   
+}else{
+
+}  
+
+
+/*
+for(c = 1; c <=6; c++){
+    console.log(c)
+    if(c >= 6){
+    console.log('Você atingiu o limite')
+    }
+}*/

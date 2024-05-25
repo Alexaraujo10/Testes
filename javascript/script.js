27be72bbebc79a1dd9c969c455b6efd0b8d0fc9e
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-25 03:33:55 -0300

    a

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+/*var pizza = 8
+do{
+    console.log(`Comer ${pizza} Pedaços`)
+    
+}while(pizza <= 8)
+    */
+
+    for(var pizza = 8; pizza<=8; c++){
+        console.log(pizza)
+    }
+    console.log('Fim')

7f7b7941ddde99505aaff1b8249d39d40634b53a
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-27 03:31:08 -0300

    a

--- modelo/teste.js
+++ modelo/teste.js
@@ -32,10 +32,15 @@ if(velocidade <= 0){
         img.setAttribute('src','tutututu du max verstappen.jpg') 
         audio.setAttribute('src', 'tu-tu-tu-du-max-verstappen.mp3') 
 
-    } else if(velocidade >= 301){
+    } else if(velocidade >= 301  && velocidade <= 998){
         res.innerHTML  = 'Eu sou a Velocidade'
         img.setAttribute('src','mcqueen.jpg')
         audio.setAttribute('src', 'eu sou a velo.mp3')
+
+    } else if(velocidade <= 999){
+        res.innerHTML  = 'Ta em um uno pae ?'
+        img.setAttribute('src', 'uno.jpg')
+        audio.setAttribute('src', 'que foguete.mp3')
     }
     res.appendChild(img)
     audio.setAttribute('autoplay','true')

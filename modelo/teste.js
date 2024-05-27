2f61c5db202cb246346655b0dd8f3173c1e0a924
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-27 01:06:08 -0300

    a

--- modelo/teste.js
+++ modelo/teste.js
@@ -2,23 +2,35 @@ function verificar(){
     let velocidade  = document.getElementById('txtv').value
     let res = document.getElementById('res')
     let speed  = Number(velocidade)
+    let img = document.createElement('img')
+    
 
 
 if(velocidade <= 0){
   res.innerHTML = 'Se ta parado irmão! se liga'
-
+  img.setAttribute('src','ta parado.jpg')
+  img.setAttribute('id', 'foto')
 } else if (velocidade >= 1 && velocidade <= 29){
         res.innerHTML = 'Você esta aprendendo ou tem entre 60 a 80 anos'
+        img.setAttribute('src','solta a embreagem.jpg')
     } 
     else if(velocidade >= 30 && velocidade <= 40){
        res.innerHTML = 'Dirija com segurança'
     } else if (velocidade >= 41 && velocidade <= 90){
         res.innerHTML = 'Vai com calma! a vida é  só  uma'
+
+        img.setAttribute('src','foto-vai com calma.jpg')
+
     } else if (velocidade >= 91 && velocidade <= 120){
         res.innerHTML = 'Ta sem freio ?'
+        img.setAttribute('src','ta sem freio.jpg')
+
     } else if (velocidade >= 121 && velocidade <= 300){
         res.innerHTML = 'Tutututu du Max Verstappen'
+        img.setAttribute('src','tutututu du max verstappen.jpg')
     } else if(velocidade >= 301){
         res.innerHTML  = 'Ta num foguete ?'
+        img.setAttribute('src','eu sou a velocidade,jpg')
     }
+    res.appendChild(img);
 }   

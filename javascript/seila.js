fe9237216d62c4a91f826319e0b2193405de80a4
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-06-06 02:50:43 -0300

    Create seila.js

---
+++ javascript/seila.js
@@ -0,0 +1,13 @@
+//var comerpizza = 1
+//var pizza = 8
+
+//do {
+//    console.log(`${comerpizza}`)
+ //   comerpizza++
+//}while( comerpizza <= 8 )
+
+  //  console.log('A pizza Acabou Fim!')
+
+
+  var arr =["1","2","3"]
+  console.log(arr[1])

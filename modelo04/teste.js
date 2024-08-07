56257111af8cd5008ce8402816930cc82430a867
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-08-07 01:36:17 -0300

    sei  lá
    Só fiz fds

---
+++ modelo04/teste.js
@@ -0,0 +1,5 @@
+function Calcular(){
+    let n1 = parseFloat(document.getElementById('n1').value)
+    let n2 = parseFloat(document.getElementById('n2').value)
+    let n3 = parseFloat(document.getElementById('n3').value)
+}

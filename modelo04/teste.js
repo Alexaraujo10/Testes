8b6175da151807d5bf6f53e12780f0ece33a50c6
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-08-21 00:56:07 -0300

    .

--- modelo04/teste.js
+++ modelo04/teste.js
@@ -1,5 +1,18 @@
-function Calcular(){
-    let n1 = parseFloat(document.getElementById('n1').value)
-    let n2 = parseFloat(document.getElementById('n2').value)
-    let n3 = parseFloat(document.getElementById('n3').value)
+function calcular() {
+    let n1 = parseFloat(document.getElementById('nota1').value)
+    let n2 = parseFloat(document.getElementById('nota2').value)
+    let n3 = parseFloat(document.getElementById('nota3').value)
+    let res = document.getElementById('res')
+
+    let soma = n1 + n2 + n3
+    let media = soma / 3
+
+    if (media >= 5) {
+        alert(`Sua media é ${media}`)
+        alert('Aprovado Parabens')
+    } else {
+        alert(`Sua media é ${media}`)
+        alert('Reprovado, Burro')
+    }
+    
 }
